70397011614ed4bc80248392d679457cc295ccf6
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2022-11-28 09:14:42 -0300

    added: AppTheme.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:xpns_trckr_app/app/app_theme.dart';
 
-/// Starting point of application, holds the MaterialApp.
+/// Starting point of application, holds the [MaterialApp].
 class XpnsTrckrApp extends StatelessWidget {
   /// Creates a new instance.
   const XpnsTrckrApp({super.key});
@@ -8,10 +9,9 @@ class XpnsTrckrApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Xpns Trckr',
+      theme: appTheme,
       home: const Scaffold(
         body: Center(
           child: Text('Xpns Trckr Apps'),
